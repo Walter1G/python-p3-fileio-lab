3c7986eeef981d268427412d12cbf22e161476f5
Walter N <2760walter@gmail.com>
2023-12-04 13:48:21 +0300

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
-
+    with open(f'{file_name}.txt', encoding='utf-8', mode='w') as text_file:
+        text_file.write(file_content)
+    
+    
 def append_file(file_name, append_content):
+    with open(f'{file_name}.txt', encoding='utf-8', mode='a') as text_file:
+        text_file.write(append_content)
     pass
 
 def read_file(file_name):
-    pass
+    with open(file_name, encoding='utf-8') as text_file:
+        text_file.read()
+        
+    
